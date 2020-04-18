d4625922c847a24856e36cd4b470895aec545cc5
Michael Lock <michalo@localhost.localdomain>
2020-04-18 21:22:17 +0300

    added and prepared required packages

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-"use strict";
+'use strict'
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const app = express()
 
-const port = 3000;
-const express = require('express');
+// ROUTES
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// app.use ROUTES
+//app.use('/cat', )
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`)
+})
